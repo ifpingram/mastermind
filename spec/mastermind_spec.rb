74926113fbdc01c966b809e1b831628f581c69a3
Ingram Monk <ingram.monk@gmail.com>
2014-09-08 13:08:19 +0100

    g

---
+++ spec/mastermind_spec.rb
@@ -0,0 +1,48 @@
+# http://www.web-games-online.com/mastermind/
+#
+# Mastermind.
+#
+# So the idea is: start out with two pegs, two colors: RG:  RR GG RG GR
+#
+# There are four color pieces: [R]ed, [G]reen, [B]lue, [Y]ellow
+#
+# You start the ruby executable.
+#
+# The program creates a random four pieces and hides them.
+#
+# You then enter your guess as four letters:  RGRG  (red-green-red-green)
+#
+# The output is then: dot=none, +=match but wrong slot, @=match and slot.
+# These are output in order: @’s, then +’s, then .’s.
+# They don’t necessarily match the order of the entry
+#
+# So: Assuming the system chose: RGRY
+#
+# > BBBB => ....
+# > RBBR => @+.. 
+# > GRYR => ++++
+# > GGRY => @@@.
+# > RGRY => @@@@
+#
+# Congrats!
+#
+# (once you get a match, it shows it and how many steps)
+#
+# Bonus: choose from possible # pieces. defaults to 4 but could be: 2 - 8:
+#
+# Red, Green, Blue, Yellow, Orange, White, Purple, Cyan, Violet.
+#
+# It should start by showing the lexicon.
+# ‘h’ should be treated as help and should show the lexicon.
+# ‘q’ should be quit
+
+class Mastermind
+
+
+end
+
+describe Mastermind do
+
+//it
+
+end
