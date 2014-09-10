f16012c5e5333fa28562a8e22284aa6b00cc1418
Ingram Monk <ingram.monk@gmail.com>
2014-09-10 14:10:01 +0100

    g

--- spec/mastermind_spec.rb
+++ spec/mastermind_spec.rb
@@ -122,6 +122,10 @@ describe Mastermind do
     it "outputs '@+.' when the input is {:match_color_and_position => 1, :match_color_not_position => 1, :no_match => 1}" do
       expect(Mastermind.new.format_counted_matches({:match_color_and_position=>1, :match_color_not_position=>1, :no_match=>1})).to eq('@+.')
     end
+
+    it "outputs '@@@+....' when the input is {:match_color_and_position => 3, :match_color_not_position => 1, :no_match => 4}" do
+      expect(Mastermind.new.format_counted_matches({:match_color_and_position=>1, :match_color_not_position=>1, :no_match=>1})).to eq('@+.')
+    end
   end
 
   context "remembering match type counts" do
