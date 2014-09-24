ece66789257b1a7eed075acbceff2663421b1048
Ingram Monk <ingram.monk@gmail.com>
2014-09-24 13:44:33 +0100

    g

--- lib/mastermind_play.rb
+++ lib/mastermind_play.rb
@@ -2,6 +2,12 @@ class MastermindPlay
   def initialize(output_stream = STDOUT)
     output_stream.puts "Welcome to Mastermind!"
     output_stream.puts 'I have created a 4 character solution for you to guess, using the following colors:'
+    output_stream.puts 'R -> Red'
+    output_stream.puts 'G -> Green'
+    output_stream.puts 'B -> Blue'
+    output_stream.puts 'Y -> Yellow'
+    output_stream.puts 'O -> Orange'
+    output_stream.puts 'Please enter your 4 character guess:'
   end
 end
 
