a1773ea132d48faeeec656fab2c3e7beb09af12e
Ingram Monk <ingram.monk@gmail.com>
2014-09-24 13:36:57 +0100

    g

--- lib/mastermind_play.rb
+++ lib/mastermind_play.rb
@@ -1,3 +1,10 @@
 class MastermindPlay
+  def initialize(output_stream = STDOUT)
+    output_stream.puts "Welcome to Mastermind!"
+  end
+end
 
+
+if __FILE__==$0
+  MastermindPlay.new # this will only run if the script was the main, not load'd or require'd
 end
--- spec/mastermind_spec.rb
+++ spec/mastermind_spec.rb
@@ -1,63 +1,6 @@
-# http://www.web-games-online.com/mastermind/
-#
-# Mastermind.
-#
-# So the idea is: start out with two pegs, two colors: RG:  RR GG RG GR
-#
-# There are four color pieces: [R]ed, [G]reen, [B]lue, [Y]ellow
-#
-# You start the ruby executable.
-#
-# The program creates a random four pieces and hides them.
-#
-# You then enter your guess as four letters:  RGRG  (red-green-red-green)
-#
-# The output is then: dot=none, +=match but wrong slot, @=match and slot.
-# These are output in order: @’s, then +’s, then .’s.
-# They don’t necessarily match the order of the entry
-#
-# So: Assuming the system chose: RGRY
-#
-# > BBBB => ....
-# > RBBR => @+.. 
-# > GRYR => ++++
-# > GGRY => @@@.
-# > RGRY => @@@@
-#
-# Congrats!
-#
-# (once you get a match, it shows it and how many steps)
-#
-# Bonus: choose from possible # pieces. defaults to 4 but could be: 2 - 8:
-#
-# Red, Green, Blue, Yellow, Orange, White, Purple, Cyan, Violet.
-#
-# It should start by showing the lexicon.
-# ‘h’ should be treated as help and should show the lexicon.
-# ‘q’ should be quit
-
 require 'mastermind'
 
 describe Mastermind do
-
-  # instantiate game
-  # generate random solution
-  # output STDIN cursor
-  # receive STDIN
-  # attempt solution
-  # output result
-
-
-  # puts "Hello! Welcome to Mastermind!"
-  # puts "Would you like to play a new game? (Y/N) "
-  # gets answer
-  # check_answer(answer)
-  # if answer == 'Y' then
-  # Mastermind.new...
-  # else
-  # puts "Sorry to hear that. Maybe next time!"
-
-
 
   context "allow duplicates in the solution" do
     [
